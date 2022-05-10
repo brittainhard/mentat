79261b7a1b3a78c78814b5c0681a17f4bb4488c3
Brittain Hard <brittainhard@gmail.com>
2022-05-09 21:57:31 -0500

    Work on a general git function.

--- bash/git.sh
+++ bash/git.sh
@@ -1,3 +1,22 @@
 gpush () {
-        git push origin $1
+	git push origin $1
+}
+
+g () {
+	if [ $1 ]
+	then
+		case $1 in
+			p)
+				gpush $2
+				;;
+			d)
+				git diff
+				;;
+			*)
+				echo "Unknown command."
+				;;
+		esac
+	else
+		echo "Not enough arguments."
+	fi
 }
--- bash/workon.sh
+++ bash/workon.sh
@@ -1,22 +1,28 @@
 export GITHUBPATH=$HOME/github
 
 open_tmux () {
-        tmux a -t $1 &> /dev/null
+	tmux a -t $1 &> /dev/null
 
-        if [ $? != 0 ]
-        then
-                tmux new -s $1
-        fi
+	if [ $? != 0 ]
+	then
+		tmux new -s $1
+	fi
         
 }
 
 workon () {
-        cd $GITHUBPATH/$1 &> /dev/null
-
-        if [ $? != 0 ]
-        then
-                echo "Project \"$1\" not found."
-        else
-                open_tmux $1
-        fi
+	if [ $1 ]
+	then
+		cd $GITHUBPATH/$1 &> /dev/null
+		if [ $? != 0 ]
+		then
+			echo "Project \"$1\" not found."
+		else
+			open_tmux $1
+		fi
+	else
+		echo "Please provide a project name."
+	fi
 }
+
+alias won="workon"
